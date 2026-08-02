39b4b36a7d9e554f681e256d78162bb55137a95e
Bethelhem Ashenafi <bettyashu5@gmail.com>
2026-08-02 22:06:50 +0300

    Allow production client CORS

--- QRMenu.Api/Program.cs
+++ QRMenu.Api/Program.cs
@@ -104,7 +104,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:5173",
-                "http://localhost:5174"
+                "http://localhost:5174",
+                "https://qrmenu-client-osgf.onrender.com"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
