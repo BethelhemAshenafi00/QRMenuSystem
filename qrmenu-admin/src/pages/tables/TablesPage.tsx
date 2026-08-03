fc2e3dfe0c48f3e6f7636071ba5e7a597879ccee
Bethelhem Ashenafi <bettyashu5@gmail.com>
2026-08-03 21:12:20 +0300

    update localhost

--- qrmenu-admin/src/pages/tables/TablesPage.tsx
+++ qrmenu-admin/src/pages/tables/TablesPage.tsx
@@ -15,7 +15,7 @@ import Button from "../../components/ui/Button";
 import Input from "../../components/ui/Input";
 import Modal from "../../components/ui/Modal";
 
-const clientBaseUrl = "http://localhost:5174/";
+const clientBaseUrl = "https://qrmenu-client-osgf.onrender.com//";
 
 function buildClientUrl(table: Table) {
   const params = new URLSearchParams();
